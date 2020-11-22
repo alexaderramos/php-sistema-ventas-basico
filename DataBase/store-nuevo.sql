e829cb8af6ccd9dc4cf1c0304c20bc3bfb6c565d
Alexander Ramos <tcristian_15.go@hotmail.com>
2020-11-22 16:17:27 -0500

    nuevo script de base de datos

---
+++ DataBase/store-nuevo.sql
@@ -0,0 +1,276 @@
+-- phpMyAdmin SQL Dump
+-- version 5.0.2
+-- https://www.phpmyadmin.net/
+--
+-- Servidor: localhost:3306
+-- Tiempo de generación: 22-11-2020 a las 21:15:54
+-- Versión del servidor: 5.7.24
+-- Versión de PHP: 7.4.6
+
+SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
+START TRANSACTION;
+SET time_zone = "+00:00";
+
+
+/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
+/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
+/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
+/*!40101 SET NAMES utf8mb4 */;
+
+--
+-- Base de datos: `store`
+--
+
+-- --------------------------------------------------------
+
+--
+-- Estructura de tabla para la tabla `administrador`
+--
+
+CREATE TABLE `administrador` (
+  `Nombre` varchar(30) NOT NULL,
+  `Clave` text NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+
+--
+-- Volcado de datos para la tabla `administrador`
+--
+
+INSERT INTO `administrador` (`Nombre`, `Clave`) VALUES
+('admin', '21232f297a57a5a743894a0e4a801fc3');
+
+-- --------------------------------------------------------
+
+--
+-- Estructura de tabla para la tabla `categoria`
+--
+
+CREATE TABLE `categoria` (
+  `CodigoCat` varchar(30) NOT NULL,
+  `Nombre` varchar(30) NOT NULL,
+  `Descripcion` text NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+
+--
+-- Volcado de datos para la tabla `categoria`
+--
+
+INSERT INTO `categoria` (`CodigoCat`, `Nombre`, `Descripcion`) VALUES
+('C1', 'Electrodomesticos', 'Articulos de primera necesidad para el hogar'),
+('C2', 'Multimedia', 'Articulos de entretenimiento y diversión'),
+('C3', 'Móbiles', 'Teléfonos celulares smartphones');
+
+-- --------------------------------------------------------
+
+--
+-- Estructura de tabla para la tabla `cliente`
+--
+
+CREATE TABLE `cliente` (
+  `NIT` varchar(30) NOT NULL,
+  `Nombre` varchar(30) NOT NULL,
+  `NombreCompleto` varchar(70) NOT NULL,
+  `Apellido` varchar(70) NOT NULL,
+  `Clave` text NOT NULL,
+  `Direccion` varchar(200) NOT NULL,
+  `Telefono` int(20) NOT NULL,
+  `Email` varchar(30) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+
+-- --------------------------------------------------------
+
+--
+-- Estructura de tabla para la tabla `detalle`
+--
+
+CREATE TABLE `detalle` (
+  `NumPedido` int(20) NOT NULL,
+  `CodigoProd` varchar(30) NOT NULL,
+  `CantidadProductos` int(20) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+
+-- --------------------------------------------------------
+
+--
+-- Estructura de tabla para la tabla `metodo_pago`
+--
+
+CREATE TABLE `metodo_pago` (
+  `id` int(10) UNSIGNED NOT NULL,
+  `venta_id` int(20) NOT NULL,
+  `numero` varchar(100) NOT NULL,
+  `expira` varchar(50) NOT NULL DEFAULT '',
+  `cvc` int(10) UNSIGNED NOT NULL DEFAULT '0',
+  `nombres` varchar(100) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+
+-- --------------------------------------------------------
+
+--
+-- Estructura de tabla para la tabla `producto`
+--
+
+CREATE TABLE `producto` (
+  `CodigoProd` varchar(30) NOT NULL,
+  `NombreProd` varchar(30) NOT NULL,
+  `CodigoCat` varchar(30) NOT NULL,
+  `Precio` decimal(30,2) NOT NULL,
+  `Modelo` varchar(30) NOT NULL,
+  `Marca` varchar(30) NOT NULL,
+  `Stock` int(20) NOT NULL,
+  `NITProveedor` varchar(30) NOT NULL,
+  `Imagen` varchar(150) NOT NULL,
+  `Nombre` varchar(30) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+
+-- --------------------------------------------------------
+
+--
+-- Estructura de tabla para la tabla `proveedor`
+--
+
+CREATE TABLE `proveedor` (
+  `NITProveedor` varchar(30) NOT NULL,
+  `NombreProveedor` varchar(30) NOT NULL,
+  `Direccion` varchar(200) NOT NULL,
+  `Telefono` int(20) NOT NULL,
+  `PaginaWeb` varchar(30) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+
+--
+-- Volcado de datos para la tabla `proveedor`
+--
+
+INSERT INTO `proveedor` (`NITProveedor`, `NombreProveedor`, `Direccion`, `Telefono`, `PaginaWeb`) VALUES
+('0001781', 'Sony', 'Minato, Tokio, Japón', 22596485, 'www.sony.com'),
+('0001782', 'HTC', 'Taoyuan, Taiwán, (República de China)', 25987456, 'www.htc.com'),
+('0001783', 'TCL', 'Huizhou, Guangdong, China', 25698745, 'tcl.com.ar'),
+('0001785', 'Samsung', 'Samsung Town, Seúl, Corea del Sur', 22504787, 'www.samsung.com'),
+('0001787', 'LG', 'Seúl,Corea del Sur', 26589874, 'www.lg.com'),
+('0001788', 'Compaq', 'Houston, Texas, EE.UU', 24569875, 'www.compaq.com');
+
+-- --------------------------------------------------------
+
+--
+-- Estructura de tabla para la tabla `venta`
+--
+
+CREATE TABLE `venta` (
+  `NumPedido` int(20) NOT NULL,
+  `Fecha` varchar(150) NOT NULL,
+  `NIT` varchar(30) NOT NULL,
+  `Descuento` int(20) NOT NULL,
+  `TotalPagar` decimal(30,2) NOT NULL,
+  `Estado` varchar(150) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+
+--
+-- Índices para tablas volcadas
+--
+
+--
+-- Indices de la tabla `administrador`
+--
+ALTER TABLE `administrador`
+  ADD PRIMARY KEY (`Nombre`);
+
+--
+-- Indices de la tabla `categoria`
+--
+ALTER TABLE `categoria`
+  ADD PRIMARY KEY (`CodigoCat`);
+
+--
+-- Indices de la tabla `cliente`
+--
+ALTER TABLE `cliente`
+  ADD PRIMARY KEY (`NIT`);
+
+--
+-- Indices de la tabla `detalle`
+--
+ALTER TABLE `detalle`
+  ADD KEY `NumPedido` (`NumPedido`),
+  ADD KEY `CodigoProd` (`CodigoProd`);
+
+--
+-- Indices de la tabla `metodo_pago`
+--
+ALTER TABLE `metodo_pago`
+  ADD PRIMARY KEY (`id`),
+  ADD KEY `FK_venta_id` (`venta_id`);
+
+--
+-- Indices de la tabla `producto`
+--
+ALTER TABLE `producto`
+  ADD PRIMARY KEY (`CodigoProd`),
+  ADD KEY `CodigoCat` (`CodigoCat`),
+  ADD KEY `NITProveedor` (`NITProveedor`),
+  ADD KEY `Agregado` (`Nombre`);
+
+--
+-- Indices de la tabla `proveedor`
+--
+ALTER TABLE `proveedor`
+  ADD PRIMARY KEY (`NITProveedor`);
+
+--
+-- Indices de la tabla `venta`
+--
+ALTER TABLE `venta`
+  ADD PRIMARY KEY (`NumPedido`),
+  ADD KEY `NIT` (`NIT`),
+  ADD KEY `NIT_2` (`NIT`);
+
+--
+-- AUTO_INCREMENT de las tablas volcadas
+--
+
+--
+-- AUTO_INCREMENT de la tabla `metodo_pago`
+--
+ALTER TABLE `metodo_pago`
+  MODIFY `id` int(10) UNSIGNED NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=2;
+
+--
+-- AUTO_INCREMENT de la tabla `venta`
+--
+ALTER TABLE `venta`
+  MODIFY `NumPedido` int(20) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=12;
+
+--
+-- Restricciones para tablas volcadas
+--
+
+--
+-- Filtros para la tabla `detalle`
+--
+ALTER TABLE `detalle`
+  ADD CONSTRAINT `detalle_ibfk_8` FOREIGN KEY (`CodigoProd`) REFERENCES `producto` (`CodigoProd`) ON UPDATE CASCADE,
+  ADD CONSTRAINT `detalle_ibfk_9` FOREIGN KEY (`NumPedido`) REFERENCES `venta` (`NumPedido`) ON DELETE CASCADE ON UPDATE CASCADE;
+
+--
+-- Filtros para la tabla `metodo_pago`
+--
+ALTER TABLE `metodo_pago`
+  ADD CONSTRAINT `FK_venta_id` FOREIGN KEY (`venta_id`) REFERENCES `venta` (`NumPedido`);
+
+--
+-- Filtros para la tabla `producto`
+--
+ALTER TABLE `producto`
+  ADD CONSTRAINT `producto_ibfk_7` FOREIGN KEY (`CodigoCat`) REFERENCES `categoria` (`CodigoCat`) ON UPDATE CASCADE,
+  ADD CONSTRAINT `producto_ibfk_8` FOREIGN KEY (`NITProveedor`) REFERENCES `proveedor` (`NITProveedor`) ON UPDATE CASCADE,
+  ADD CONSTRAINT `producto_ibfk_9` FOREIGN KEY (`Nombre`) REFERENCES `administrador` (`Nombre`);
+
+--
+-- Filtros para la tabla `venta`
+--
+ALTER TABLE `venta`
+  ADD CONSTRAINT `venta_ibfk_1` FOREIGN KEY (`NIT`) REFERENCES `cliente` (`NIT`) ON UPDATE CASCADE;
+COMMIT;
+
+/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
+/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
+/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
